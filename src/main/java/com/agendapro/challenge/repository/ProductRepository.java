bb424e3a2cebcc28ad64190dba58fac5142faa4d
maorellanag <maorellana92@gmail.com>
2024-12-02 00:25:59 -0300

    fuzzy search

--- src/main/java/com/agendapro/challenge/repository/ProductRepository.java
+++ src/main/java/com/agendapro/challenge/repository/ProductRepository.java
@@ -1,7 +1,9 @@
 package com.agendapro.challenge.repository;
 
 import com.agendapro.challenge.domain.Product;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -9,5 +11,7 @@ public interface ProductRepository extends CrudRepository<Product, Long> {
 
     List<Product> findAll();
 
-    List<Product> findByName(String name);
+    @Query(value = "SELECT * FROM product p WHERE LOWER(p.name) LIKE LOWER(CONCAT('%', :name, '%'))", nativeQuery = true)
+    List<Product> findByName(@Param("name") String name);
+
 }
